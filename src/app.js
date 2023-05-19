a867e4490464bde7c29f5779bf5d8988840adfd3
Max Tauroza <maxtauroza@gmail.com>
2023-05-19 10:10:01 +0100

    added an easy feed option using patch

--- src/app.js
+++ src/app.js
@@ -7,8 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.post('/cats', (req, res) => {
-    Cat.create(req.body).then(cat => res.status(201).json(cat))
-});
+    Cat.create(req.body).then(cat => res.status(201).json(cat))});
 
 app.get('/cats', (req, res) => {
     Cat.findAll({ where: req.query }).then(catList => res.status(200).json(catList))
@@ -20,6 +19,10 @@ app.get('/cats/:id', (req, res) => {
 app.patch('/cats/:id', (req, res) => {
     const { id } = req.params
     Cat.update(req.body, { where: { id: `${id}` }}).then(fedCat => res.status(200).json(fedCat))
+})
+app.patch('/feed/cats/:id', (req, res) => {
+    const { id } = req.params
+    Cat.update({ lastFed: new Date()}, { where: { id: `${id}`}}).then(easyFeedCat => res.status(200).json(easyFeedCat))
 })
 app.delete('/cats/:id', (req, res) => {
     const { id } = req.params
